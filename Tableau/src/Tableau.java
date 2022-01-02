76c799aadb6d06e1e3cafa193072c89c74fbe25f
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-02 19:34:34 +0100

    ajout dossier tableau

---
+++ Tableau/src/Tableau.java
@@ -0,0 +1,16 @@
+public class Tableau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []car;
+		int n;
+		System.out.print("combien de valeurs : ");
+		n= Clavier.lireInt();
+		car= new int[n];
+		for (int i=0; i<n; i++)
+		car[i] = (2*i+1)*(2*i+1);
+		for (int i=0; i<n;i++)
+		System.out.println((2*i+1)+ " a pour carre "+ car[i] );
+	}
+
+}
